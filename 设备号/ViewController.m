7896c8e5069cf2aa450aa1b5ceccd749d099345f
Never <Never1750@163.com>
2016-01-30 00:44:59 +0800

    修改颜色

--- 设备号/ViewController.m
+++ 设备号/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithRed:0.4 green:1.0 blue:1.0 alpha:1.0];
+    
     
 }
 
